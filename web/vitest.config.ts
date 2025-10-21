39a9b5282f8d81243b09f55dd5ffbeef3c1771db
Fatdevil <Stellanjonsson1976@gmail.com>
2025-10-21 19:07:14 +0200

    test(qa): move ghost telemetry spec to web tests (#210)

--- web/vitest.config.ts
+++ web/vitest.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
       'src/__tests__/**/*.spec.ts',
       '../shared/playslike/__tests__/**/*.spec.ts',
       '../shared/runs/__tests__/**/*.spec.ts',
-      '../golfiq/app/src/screens/utils/__tests__/**/*.spec.ts'
+      'tests/**/*.spec.ts'
     ]
   }
 });
